5893c7753be3920d897f68de5a8cc833421f1aac
Cristian Gadea <cristianhernangadea@gmail.com>
2021-06-03 19:56:54 -0300

    Added NavBar in components

---
+++ src/components/NavBar.js
@@ -0,0 +1,41 @@
+import React, { Component } from "react";
+import '../App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Navbar,  NavDropdown, Nav, Form, FormControl, Button} from 'react-bootstrap';
+
+class NavBar extends Component {
+    render() {
+      return (
+        <Navbar bg="light" expand="lg">
+            <Navbar.Brand href="#home">EcoHouse</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="mr-auto">
+                    <Nav.Link href="#home">Products</Nav.Link>
+                    <NavDropdown title="Categories" id="basic-nav-dropdown">
+                        <NavDropdown.Item href="#action/3.1">Foods</NavDropdown.Item>
+                        <NavDropdown.Item href="#action/3.2">Drinks</NavDropdown.Item>
+                        <NavDropdown.Item href="#action/3.3">Clothes</NavDropdown.Item>
+                        <NavDropdown.Item href="#action/3.4">Technology</NavDropdown.Item>
+                        <NavDropdown.Divider />
+                        <NavDropdown.Item href="#action/3.5">House</NavDropdown.Item>
+                    </NavDropdown>
+                    <Nav.Link href="#link">Favorities</Nav.Link>
+                    <NavDropdown title="Account" id="basic-nav-dropdown">
+                        <NavDropdown.Item href="#action/3.6">Info</NavDropdown.Item>
+                        <NavDropdown.Divider />
+                        <NavDropdown.Item href="#action/3.7">Sign Out</NavDropdown.Item>
+                    </NavDropdown>
+
+                </Nav>
+                <Form inline>
+                    <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+                    <Button variant="outline-success">Search</Button>
+                </Form>
+            </Navbar.Collapse>
+      </Navbar>
+      );
+    }
+  }
+  
+  export default NavBar;
