af5a475caed4c8e0a33857f6136b44c52b7bc118
Cristian Gadea <cristianhernangadea@gmail.com>
2021-07-21 16:25:45 -0300

    Changed ItemCount

---
+++ src/components/ItemCount.js
@@ -0,0 +1,47 @@
+import React, { Component} from "react";
+import '../App.css';
+
+class ItemCount extends Component{
+    stock = this.props.stock;
+    constructor(props) {
+        super(props);
+        this.state = {in: props.inicial};
+      }
+
+    addOn = () => {
+        const stock = parseInt(document.getElementById("count").value);
+        if (this.stock >= stock){
+            alert("Producto/s agregado/s");
+        }
+    }
+    incrementar=()=>{
+        let input = document.getElementById("count");
+        if (input.value<this.stock){
+            // eslint-disable-next-line react/no-direct-mutation-state
+            this.setState({in:(++this.state.in)});
+            input.value = this.state.in;
+        }
+    }
+    decrementar=()=>{
+        let input = document.getElementById("count");
+        if (input.value>this.props.inicial){
+        // eslint-disable-next-line react/no-direct-mutation-state
+        this.setState({in:(--this.state.in)});
+        input.value= this.state.in;       
+        }else{
+             input.value= this.props.inicial;
+        }
+    }
+    render() {
+        return(
+           <div>
+                <input type="number" id="count" defaultValue={this.state.in}></input>
+                <button onClick={this.incrementar}>+</button>
+                <button onClick={this.decrementar}>-</button>
+                <button onClick={this.addOn}>Realizar Pedido</button>
+            </div> 
+        );
+    }
+}
+
+export default ItemCount;
